519b5d0ad033b577bb02867d494978803924c03b
Andrey Helldar <helldar@dragon-code.pro>
2023-03-30 17:41:32 +0300

    Upgrade locales

--- locales/fr/back.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-return [
-    'simple' => 'retour',
-    'home' => "retour à l'accueil",
-    'top' => 'retour en haut',
-    'list' => 'retour à la liste',
-    'something' => 'retour :something',
-];
--- locales/fr/number.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-return [
-
-    'decimals_separator' => ',',
-    'thousands_separator' => ' ',
-
-];

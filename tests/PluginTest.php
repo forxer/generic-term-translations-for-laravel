65957d0bd74b57cb7af23e83028567b4e7ebf064
Vincent Garnier <forxer@gmail.com>
2026-08-26 15:34:09 +0000

    test: assert en locale values match source files

--- tests/PluginTest.php
+++ tests/PluginTest.php
@@ -9,4 +9,19 @@ use LaravelLang\StatusGeneratorTests\TestCase as BaseTestCase;
 class PluginTest extends BaseTestCase
 {
     protected string $base_path = __DIR__.'/../';
+
+    public function test_source_values_match_en_locale(): void
+    {
+        $locale = json_decode(file_get_contents(__DIR__.'/../locales/en/php.json'), true);
+
+        foreach (glob(__DIR__.'/../source/*.php') as $file) {
+            foreach (require $file as $key => $value) {
+                $this->assertSame(
+                    $value,
+                    $locale[$key] ?? null,
+                    \sprintf('Key "%s" from %s does not match the en locale.', $key, basename($file))
+                );
+            }
+        }
+    }
 }
